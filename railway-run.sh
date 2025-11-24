f859ff8fd0140a83cbc119a09257635fd341b428
blandd-coffee <blandd.dev@gmail.com>
2025-11-24 13:36:32 -0500

    feat: add railway run script to start the server application

---
+++ railway-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Railway run script to start the server application
+
+# Change to the server directory
+cd server
+
+# Install dependencies
+npm install
+
+# Start the application
+node app.js
